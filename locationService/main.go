daa34399832eb5b6eb09f0a1de48f69f8c4b7f59
Joakim Gustin <joakim.gustin@collectorbank.se>
2016-08-31 13:42:45 +0200

    Fix node client example and ability to disable TLS

--- locationService/main.go
+++ locationService/main.go
@@ -13,7 +13,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 
-	pb "github.com/gust1n/microServicesArchitecture/locationService/locationservice"
+	pb "github.com/gust1n/malabar/locationService/locationservice"
 )
 
 type locationServiceServer struct{}
@@ -21,7 +21,7 @@ type locationServiceServer struct{}
 func (lss *locationServiceServer) TrackUser(user *pb.User, stream pb.LocationService_TrackUserServer) error {
 	for i := 0; i < 100; i++ {
 		if err := stream.Send(&pb.Point{
-			Latitude: 1,
+			Latitude:  1,
 			Longitude: 0,
 		}); err != nil {
 			return err
@@ -40,30 +40,34 @@ func main() {
 		listenAddr = flag.String("listen-addr", "0.0.0.0:9000", "HTTP listen address.")
 		tlsCert    = flag.String("tls-cert", withConfigDir("cert.pem"), "TLS server certificate.")
 		tlsKey     = flag.String("tls-key", withConfigDir("key.pem"), "TLS server key.")
+		enableTLS  = flag.Bool("tls", true, "Enable TLS")
 	)
 	flag.Parse()
 
 	log.Println("Location service starting...")
 
-	cert, err := tls.LoadX509KeyPair(*tlsCert, *tlsKey)
-	if err != nil {
-		log.Fatal(err)
+	var opts []grpc.ServerOption
+	if *enableTLS {
+		cert, err := tls.LoadX509KeyPair(*tlsCert, *tlsKey)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		rawCaCert, err := ioutil.ReadFile(*caCert)
+		if err != nil {
+			log.Fatal(err)
+		}
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(rawCaCert)
+
+		creds := credentials.NewTLS(&tls.Config{
+			Certificates: []tls.Certificate{cert},
+			ClientCAs:    caCertPool,
+			ClientAuth:   tls.RequireAndVerifyClientCert,
+		})
+		opts = []grpc.ServerOption{grpc.Creds(creds)}
 	}
-
-	rawCaCert, err := ioutil.ReadFile(*caCert)
-	if err != nil {
-		log.Fatal(err)
-	}
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(rawCaCert)
-
-	creds := credentials.NewTLS(&tls.Config{
-		Certificates: []tls.Certificate{cert},
-		ClientCAs:    caCertPool,
-		ClientAuth:   tls.RequireAndVerifyClientCert,
-	})
-
-	gs := grpc.NewServer(grpc.Creds(creds))
+	gs := grpc.NewServer(opts...)
 	pb.RegisterLocationServiceServer(gs, &locationServiceServer{})
 
 	lis, err := net.Listen("tcp", *listenAddr)
